1d6beff3a742e44dd99d1d1029b7d23b604a8b71
shaoling <sshling@gmail.com>
2014-11-23 11:25:07 +0800

    t

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 spring-common-mybatisGen
 ========================
 开发中.....
+待开发：
+	支持Spring+JPA注解
+	支持Spring+XML配置
+	集成分页插件
 java快速开发框架：bboss
---
+++ src/main/java/cn/shaolingweb/framework/data/page/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * PageHelper相关帮助类
+ * 主要是Mybatis相关的分页
+ * @author  shaoling@shaolingweb.cn
+ */
+package cn.shaolingweb.framework.data.page;
